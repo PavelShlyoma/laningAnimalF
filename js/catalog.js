0af1f6e9ac6e94b1da46117d94c1b11751f2ec1e
Pavel Shlyoma <pashcko.shlyoma@yandex.ru>
2025-07-07 22:39:16 +0300

    bag-shopping list create, html

--- js/catalog.js
+++ js/catalog.js
@@ -14,3 +14,96 @@ function openCatalog(evt, tabname) {
     openTabs.className += " active";
     evt.currentTarget.className += " active";
 }
+
+// START menu/ mobile-menu
+
+const getButtonsElements = document.querySelectorAll('.hamburger');
+
+const getMenuElement = document.getElementById('menu');
+
+const getMenuItem = document.querySelector('.menu');
+
+function setMenuActive(i) {
+    getButtonsElements[i].classList.toggle('active');
+    getMenuElement.classList.toggle('active');
+    getMenuItem.classList.toggle('menu-active')
+}
+
+getMenuElement.addEventListener('click', (event) => {
+
+    let getId = event.target.closest('.menu');
+
+    if (!getId) {
+        setMenuActive(0);
+    }
+})
+
+// END menu/ mobile-menu
+
+const getButtonsElemntsIcons = document.getElementById('buttonLog')
+
+const getMenuUserLogElement = document.getElementById('menuUserLog');
+
+const getMenuUserLog = document.querySelector('.menu-user');
+
+getButtonsElemntsIcons.addEventListener('click', () => {
+    getMenuUserLogElement.classList.toggle('active');
+    getMenuUserLog.classList.toggle('active');
+})
+
+getMenuUserLogElement.addEventListener('click', (event) => {
+
+    let getButtonId = event.target.closest('.hamburger');
+
+    if (getButtonId) {
+        getMenuUserLogElement.classList.toggle('active');
+        getMenuUserLog.classList.toggle('active');
+    }
+
+    let getId = event.target.closest('.menu-user');
+
+    if (!getId) {
+        getMenuUserLogElement.classList.toggle('active');
+        getMenuUserLog.classList.toggle('active');
+    }
+
+})
+
+
+// START bag-shopping
+
+const getButtonsElemntsIconsShop = document.getElementById('buttonBag')
+
+console.log(getButtonsElemntsIconsShop);
+
+const getElementShop = document.getElementById('bagShopping');
+
+console.log(getElementShop);
+
+const getBagShop= document.querySelector('.bag-shopping');
+
+console.log(getBagShop);
+
+getButtonsElemntsIconsShop.addEventListener('click', () => {
+    getElementShop.classList.toggle('active');
+    getBagShop.classList.toggle('active');
+})
+
+getElementShop.addEventListener('click', (event) => {
+
+    let getButtonId = event.target.closest('.close-button-item');
+
+    if (getButtonId) {
+        getElementShop.classList.toggle('active');
+        getBagShop.classList.toggle('active');
+    }
+
+    let getId = event.target.closest('.bag-shopping');
+
+    if (!getId) {
+        getElementShop.classList.toggle('active');
+        getBagShop.classList.toggle('active');
+    }
+
+})
+
